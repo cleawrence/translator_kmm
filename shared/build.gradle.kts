f7fad52560a93709be8587d0eb82255fcb651f4d
DevClark <e.cleawrence@gmail.com>
2023-01-13 12:03:05 +0100

    add fonts, drawable configure buildsrc and build.gradle.kts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
     kotlin("multiplatform")
+    kotlin("native.cocoapods")
     id("com.android.library")
+    kotlin("plugin.serialization") version Deps.kotlinVersion
+    id("com.squareup.sqldelight")
 }
 
 kotlin {
     android()
-    
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -13,17 +16,34 @@ kotlin {
     ).forEach {
         it.binaries.framework {
             baseName = "shared"
+            version = "1.O"
         }
     }
 
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            dependencies {
+                implementation(Deps.ktorCore)
+                implementation(Deps.ktorSerialization)
+                implementation(Deps.ktorSerializationJson)
+                implementation(Deps.sqlDelightRuntime)
+                implementation(Deps.sqlDelightCoroutinesExtensions)
+                implementation(Deps.kotlinDateTime)
+            }
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
+                implementation(Deps.assertK)
+                implementation(Deps.turbine)
             }
         }
-        val androidMain by getting
+        val androidMain by getting {
+            dependencies {
+                implementation(Deps.ktorAndroid)
+                implementation(Deps.sqlDelightAndroidDriver)
+            }
+        }
         val androidTest by getting
         val iosX64Main by getting
         val iosArm64Main by getting
@@ -33,6 +53,10 @@ kotlin {
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation(Deps.ktorIOS)
+                implementation(Deps.sqlDelightNativeDriver)
+            }
         }
         val iosX64Test by getting
         val iosArm64Test by getting
@@ -48,9 +72,9 @@ kotlin {
 
 android {
     namespace = "com.appiwedia.apps.android.translator_kmm"
-    compileSdk = 32
+    compileSdk = 33
     defaultConfig {
         minSdk = 28
-        targetSdk = 32
+        targetSdk = 33
     }
 }
